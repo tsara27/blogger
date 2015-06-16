584fac8ebafad7baeae0135bce12c1571a1f35bb
tsara27 <tsara.tf@gmail.com>
2015-06-17 06:13:58 +0700

    move phone and email to contact router

--- app/router.js
+++ app/router.js
@@ -8,9 +8,10 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource('posts', { path: "/" });
   this.resource('about');
-  this.resource('contact');
-  this.resource('phone');
-  this.resource('email');
+  this.resource('contact', function(){
+    this.resource('phone');
+    this.resource('email');
+  });
 });
 
 export default Router;
